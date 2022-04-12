a94fb7fa815e478df0b020e2296249b8986a238f
Robi Setiawan <robbysetiawan2000@gmail.com>
2022-04-12 19:16:31 +0700

    16. User Login & Middleware

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Selamat DAtang, robi</h1>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -20,11 +20,35 @@
                     <a class="nav-link {{ $title === 'Posts' ? 'active' : '' }}" href="/categories">Category</a>
                 </li>
             </ul>
+
             <ul class="navbar-nav ms-auto">
+                @auth
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Welcome back, {{ auth()->user()->name }}
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="/dashboard">My Dashboard</a></li>
+
+                            <hr class="dropdown-divider">
+                    </li>
+                    <li>
+                        <form action="/logout" method="post">
+                            @csrf
+                            <button type="submit" class="dropdown-item">Logout</button>
+                        </form>
+
+                    </li>
+                </ul>
+                </li>
+            @else
                 <li class="nav-item">
                     <a href="/login" class="nav-link"><i class="bi bi-box-arrow-in-right"></i> Login</a>
                 </li>
-            </ul>
+                </ul>
+            @endauth
+
         </div>
     </div>
 </nav>
