4efc16d26bd609dd5c2e7535f8ae507c43ea5346
Robi Setiawan <robbysetiawan2000@gmail.com>
2022-04-13 20:40:31 +0700

    20. Update & Delete Post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -4,10 +4,15 @@
     <article>
         <h2 class="mb-3">{{ $post->title }}</h2>
         <a href="/dashboard/posts" class="btn btn-success">Back to my Posts</a>
-        <a href="" class="btn btn-warning">Edit</a>
-        <a href="" class="btn btn-danger">Delete</a>
+        <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning">Edit</a>
+        <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+            @method('delete')
+            @csrf
+            <button class="btn btn-danger border-0" onclick="return confirm ('Are You sure')">Delete <span
+                    data-feather="x-circle"></span></button>
+        </form>
         <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="card-img-top mt-3" alt="...">
         {!! $post->body !!} {{-- menjalankan script html jg --}}
     </article>
-    <a href="/dashboard/posts">kembali ke Post</a>
+    <a href="/dashboard/posts" class="btn btn-primary mt-2 mb-5">kembali ke Post</a>
 @endsection
