cfb403ea34b4959230cc0d40fd799d459d80d155
Robi Setiawan <robbysetiawan2000@gmail.com>
2022-04-13 11:37:46 +0700

    18. Create Post Form

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -6,6 +6,7 @@
     </div>
 
     <div class="table-responsive col-lg-8">
+        <a href="/dashboard/posts/create" class="btn btn-primary">Create New Post</a>
         <table class="table table-striped table-sm">
             <thead>
                 <tr>
